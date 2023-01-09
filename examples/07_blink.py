4481588279cef6961b5dce5d0f5d29b2069f7343
rivasalvaroy <rivasalvaroy@gmail.com>
2023-01-09 12:47:06 -0600

    ex_005 - blink the on-board led, p. 48

---
+++ examples/07_blink.py
@@ -0,0 +1,12 @@
+import machine
+import utime
+
+# for RPP  (the on-board LED)
+led_onboard = machine.Pin(25, machine.Pin.OUT)
+# for RPP W (the on-board LED)
+# led_onboard = machine.Pin("LED", machine.Pin.OUT)
+while True:
+    led_onboard.value(1)
+    utime.sleep(5)
+    led_onboard.value(0)
+    utime.sleep(5)
---
+++ examples/08_blink.py
@@ -0,0 +1,10 @@
+import machine
+import utime
+
+# for RPP  (the on-board LED)
+led_onboard = machine.Pin(25, machine.Pin.OUT)
+# for RPP W (the on-board LED)
+# led_onboard = machine.Pin("LED", machine.Pin.OUT)
+while True:
+    led_onboard.toggle()
+    utime.sleep(5)
