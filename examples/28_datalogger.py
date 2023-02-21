77a536af8505086ad842f4b87b01b6667971ca5d
rivasalvaroy <rivasalvaroy@gmail.com>
2023-02-20 21:57:09 -0600

    add reference ./imgs/01_breadboard.png

--- examples/28_datalogger.py
+++ examples/28_datalogger.py
@@ -1,8 +1,8 @@
+# ./imgs/01_breadboard.png
 import machine
 import utime
 
-sensor_temp = machine.ADC(machine.ADC.CORE_TEMP)    # type:ignore
-
+sensor_temp = machine.ADC(machine.ADC.CORE_TEMP) # type: ignore
 conversion_factor = 3.3 / (65535)
 reading = sensor_temp.read_u16() * conversion_factor
 temperature = 27 - (reading - 0.706)/0.001721
