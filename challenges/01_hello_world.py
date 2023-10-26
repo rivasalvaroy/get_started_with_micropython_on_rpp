61e7d07546b0793c987fe0ae21ea5a5a2253c46f
rivasalvaroy <rivasalvaroy@gmail.com>
2023-10-26 16:37:28 -0600

    Add hello_world.py ch02 challenge: new message & breadboard image

---
+++ challenges/01_hello_world.py
@@ -0,0 +1,17 @@
+'''
+CHALLENGE: NEW MESSAGE
+./images_breadboard/01_breadboard.png
+
+1. Can you change the message the Python program prints as its output? 
+2. If you wanted to add more messages, would you use interactive mode or script mode? 
+3. What happens if you remove the brackets or the quotation marks from the program and then try to run it again?
+'''
+
+# 1
+print("Good Bye, World!")
+
+# 2
+# script mode
+
+# 3
+# SyntaxError: invalid syntax
