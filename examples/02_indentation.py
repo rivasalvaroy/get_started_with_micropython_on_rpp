24319dcf78c3a5dcf38ed1815952f6b8a79a2901
rivasalvaroy <rivasalvaroy@gmail.com>
2023-10-26 17:01:04 -0600

    Add indentation.py & while.py ch02 example

---
+++ examples/02_indentation.py
@@ -0,0 +1,6 @@
+# ./images_breadboard/01_breadboard.png
+print("Loop starting!")
+for i in range(10):
+    print("Loop number", i)
+    print('Hello')
+print("Loop finished!")
---
+++ examples/03_while.py
@@ -0,0 +1,8 @@
+# ./images_breadboard/01_breadboard.png
+import utime
+
+print("Loop starting!")
+while True:
+    print("Loop running!")
+    utime.sleep(1)
+print("Loop finished!")
