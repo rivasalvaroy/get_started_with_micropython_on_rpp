1e81a7b8a017bb39c50582ae5bd7fb04e629b28d
rivasalvaroy <rivasalvaroy@gmail.com>
2023-10-26 22:32:21 -0600

    Add name_test.py all variants ch02 example

---
+++ examples/04_name_test.py
@@ -0,0 +1,2 @@
+# ./images_breadboard/01_breadboard.png
+user_name = input("What is your name? ")
---
+++ examples/05_name_test.py
@@ -0,0 +1,6 @@
+# ./images_breadboard/01_breadboard.png
+user_name = input("What is your name? ")
+if user_name == "Clark Kent":
+    print("You are Superman!")
+else:
+    print("You are not Superman!")
