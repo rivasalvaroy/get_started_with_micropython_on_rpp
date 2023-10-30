56bd28184935ed582447bf482136a3896e1e85ca
rivasalvaroy <rivasalvaroy@gmail.com>
2023-10-29 23:00:30 -0600

    Add switch.py ch04 example & breadboard image

---
+++ examples/12_switch.py
@@ -0,0 +1,12 @@
+# ./images_breadboard/04_breadboard.png
+import machine
+import utime
+
+led_external = machine.Pin(15, machine.Pin.OUT)
+button = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
+
+while True:
+    if button.value() == 1:
+        led_external.value(1)
+        utime.sleep(2)
+    led_external.value(0)
