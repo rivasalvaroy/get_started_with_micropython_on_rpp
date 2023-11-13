5212612709ee5108456713b52d2cec7a8ec48a74
rivasalvaroy <rivasalvaroy@gmail.com>
2023-11-13 12:19:00 -0600

    Add blugar_alarm.py all variants ch07 example & breadboard image

---
+++ examples/22_burglar_alarm.py
@@ -0,0 +1,25 @@
+# ./images_breadboard/10_breadboard.png
+import machine
+import utime
+
+sensor_pir = machine.Pin(28, machine.Pin.IN, machine.Pin.PULL_DOWN)
+led = machine.Pin(15, machine.Pin.OUT)
+buzzer = machine.Pin(14, machine.Pin.OUT)
+
+
+def pir_handler(pin):
+    utime.sleep_ms(100)
+    if pin.value():
+        print("ALARM! Motion detected!")
+        for i in range(50):
+            led.toggle()
+            for j in range(25):
+                buzzer.toggle()
+                utime.sleep_ms(3)
+
+
+sensor_pir.irq(trigger=machine.Pin.IRQ_RISING, handler=pir_handler)
+
+while True:
+    led.toggle()
+    utime.sleep(5)
